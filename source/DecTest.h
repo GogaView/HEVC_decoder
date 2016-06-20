ea5dafed5fe80c50f077642ab2b4e1ab5ed86464
GogaView <ksa2204@yandex.ru>
2016-06-20 22:01:40 +0300

    First commit

---
+++ source/DecTest.h
@@ -0,0 +1,13 @@
+//
+//  DecTest.h
+//  HM
+//
+//  Created by MacBook on 20.03.16.
+//
+//
+
+#ifndef HM_DecTest_h
+#define HM_DecTest_h
+
+struct int st_DecodBloks;
+#endif
---
+++ source/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  HM
+//
+//  Created by MacBook on 20.03.16.
+//
+//
+
+#ifndef HM_Test_h
+#define HM_Test_h
+
+static int st_BlocksCount;
+
+#endif
